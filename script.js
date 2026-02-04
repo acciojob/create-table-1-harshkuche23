c6b5d22020b1aaeb5aedaf71301b76da733b2ead
harshkuche23 <harshkuche23@gmail.com>
2026-02-04 23:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  function insert_Row() {
     const table = document.getElementById("sampleTable");
 
-    // Insert a new row at the top (index 0)
+    // insert row at top
     const newRow = table.insertRow(0);
 
-    // Insert two cells
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
 
-    // Set cell values
     cell1.innerHTML = "New Cell1";
     cell2.innerHTML = "New Cell2";
-}
-
-  
 }
